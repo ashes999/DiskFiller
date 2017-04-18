f4363da756c8f4a96bee8beb9430c596cf9d25d8
ashes999 <ashes999@yahoo.com>
2017-04-18 17:26:14 -0400

    DiskFiller v1.0

---
+++ DiskFiller/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace DiskFiller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(197, 38);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Fill \'er up!";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(128, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(144, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Location to dump files";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(12, 38);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Stop!";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(12, 186);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(260, 47);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "This tool fills the specified folder with garbage data. It creates one file per C" +
+    "PU. For best results, try a VM with 2 CPUs (~1800MB per minute)";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(12, 74);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 13);
+            this.lblStatus.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 242);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Fill up a drive, fast.";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ DiskFiller/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DiskFiller
+{
+    public partial class Form1 : Form
+    {
+        private string rootPath;
+        private bool stop = false;
+        private List<Thread> Threads = new List<Thread>();
+        private Stopwatch stopwatch;
+        private int bytesWritten = 0;
+        private System.Timers.Timer timer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.textBox1.Text = Directory.GetCurrentDirectory();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.stop = false;
+            this.bytesWritten = 0;
+            rootPath = this.textBox1.Text;
+            var numThreads = System.Environment.ProcessorCount;
+
+            for (int i = 0; i < numThreads; i++)
+            {
+                this.spawnThread();
+            }
+
+            MessageBox.Show("Spawned " + numThreads + " threads.");
+
+            this.stopwatch = new Stopwatch();
+            this.timer = new System.Timers.Timer();
+            this.timer.Interval = TimeSpan.FromSeconds(1).TotalMilliseconds;
+            this.timer.Elapsed += (o, e2) =>
+            {
+                int mb = 60 * this.bytesWritten / 1024 / 1024;
+                this.lblStatus.Text = string.Format("Writing {0}mb per minute", mb);
+                this.bytesWritten = 0;
+            };
+            this.timer.Start();
+        }
+
+        private void spawnThread()
+        {
+            var thread = new Thread(new ThreadStart(() =>
+            {
+                // Random path
+                string myPath = Path.Combine(rootPath, Guid.NewGuid().ToString());
+                while (stop == false)
+                {
+                    using (FileStream f = new FileStream(myPath, FileMode.Append, FileAccess.Write))
+                    {
+                        using (StreamWriter s = new StreamWriter(f))
+                        {
+                            s.Write(RandomString(1024378)); // seems to give the best performance in terms of bytes per second
+                            this.bytesWritten += 1024378;
+                        }
+                    }
+                }
+            }));
+
+            thread.Start();
+            this.Threads.Add(thread);
+        }
+
+        private static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var random = new Random();
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.timer.Stop();
+            this.stop = true;
+            foreach (var thread in this.Threads)
+            {
+                thread.Abort();
+            }
+
+            MessageBox.Show("Stopped.");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
